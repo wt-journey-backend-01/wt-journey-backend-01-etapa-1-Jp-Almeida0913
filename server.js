cdcc74d334d6c2a4201953087946619e3f3df767
Joao Pedro Almeida Moreira <joaoalmeida0913@outlook.com>
2025-07-12 19:55:27 -0300

    Algumas alterações e avanços no projeto

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-const express = require('express');
+const express = require(`express`);
+const path = require(`path`);
+const fs = require(`fs`)
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'views')));
+
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.sendFile(path.join(__dirname, `views`, `index.html`))
 });
+
+app.get('/sugestao', (req, res) => {
+    res.sendFile(path.join(__dirname, `views`, `sugestao.html` ))
+})
+
+app.get('/contato', (req, res) => {
+    res.sendFile(path.join(__dirname, `views`, `contato.html` ))
+})
+
+app.post('/contato', (req, res) => {
+    res.sendFile(path.join(__dirname, `views`, `contato.html` ))
+})
+
+app.get('/api/lanches', (req, res) => {
+    res.sendFile(path.join(__dirname, `views`, `contato.html` ))
+})
+
+app.get('/')
 
 app.listen(PORT, () => {
     console.log(`Servidor da DevBurger rodando em localhost:${PORT}`);
 });
+
+
